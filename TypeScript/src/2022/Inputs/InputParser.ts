6f87a27c784256232273297401081a7b0e7c827d
cdgraybill <cdgraybill@gmail.com>
2024-01-03 10:56:57 -0600

    day 1

--- TypeScript/src/2022/Inputs/InputParser.ts
+++ TypeScript/src/2022/Inputs/InputParser.ts
@@ -1,17 +1,8 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 function parseToStrings(input: string) {
-    var text = readFileSync(input).toString('utf-8');
-    return text.split("\n")
+  var text = readFileSync(input).toString();
+  return text.split("\r\n");
 }
 
-function parseToInts(input: string) {
-    var text = readFileSync(input).toString('utf-8').split("\r\n");
-    var numbers = text.forEach(element => {
-        
-        parseInt(element);
-    });
-    return numbers;
-}
-
-export {parseToStrings, parseToInts};
+export { parseToStrings };
--- TypeScript/src/2022/Solvers/Day01Solver.ts
+++ TypeScript/src/2022/Solvers/Day01Solver.ts
@@ -1,8 +1,22 @@
-import { parseToInts, parseToStrings } from "../Inputs/InputParser.js";
+import { parseToStrings } from "../Inputs/InputParser.js";
 
 function Day01Part01() {
-    var bags = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
-    return bags;
+    var snacks = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
+    var largestBag = 0;
+    var temp = 0;
+
+    for (let i = 0; i < snacks.length; i++) {
+        if (snacks[i] === '' && temp > largestBag) {
+            largestBag = temp;
+            temp = 0;
+            continue;
+        } else {
+            var snack = parseInt(snacks[i]);
+            temp += snack;
+        }
+    }
+
+    return largestBag;
 } 
 
 console.log(Day01Part01());
