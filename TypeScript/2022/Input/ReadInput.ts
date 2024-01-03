337808095efc66656dde86cc36cf92bb5695c12d
cdgraybill <cdgraybill@gmail.com>
2024-01-02 21:26:46 -0600

    nuking ts folder

--- TypeScript/2022/Input/ReadInput.ts
+++
@@ -1,8 +0,0 @@
-import {readFileSync} from "fs";
-
-function parseInput(filePath: string) {
-    var text = readFileSync(filePath).toString('utf-8');
-    return text.split("\n")
-} 
-
-export default {parseInput};
--- TypeScript/2022/Solvers/Day01Solver.ts
+++
@@ -1,7 +0,0 @@
-import ReadInput from "../Input/ReadInput";
-
-function SolveDay01Part01() {
-    var bags = ReadInput.parseInput("./TypeScript/2022/Input/Day01Input.txt");
-};
-
-console.log(SolveDay01Part01);
