fab6f603274a6ec31b046b6d3c29c7b4614a7e2e
cdgraybill <cdgraybill@gmail.com>
2024-01-05 10:33:53 -0600

    day1 part1

--- TypeScript/src/2022/Solvers/Day01Solver.ts
+++ TypeScript/src/2022/Solvers/Day01Solver.ts
@@ -2,19 +2,19 @@ import { parseToStrings } from "../Inputs/InputParser.js";
 
 function Day01Part01() {
     var snacks = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
-    var largestBag = 0;
     var temp = 0;
+    var bags = [];
 
     for (const snack of snacks) {
-        if (snack.length === 0 && temp > largestBag) {
-            largestBag = temp;
+        if (snack.length === 0) {
+            bags.push(temp);
             temp = 0;
         } else {
             temp += parseInt(snack);
         }
     }
 
-    return largestBag;
+    return Math.max.apply(0, bags);
 } 
 
 console.log(Day01Part01());
