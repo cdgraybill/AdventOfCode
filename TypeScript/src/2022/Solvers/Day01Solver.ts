7b656e510acb064b7e58d3c3d47071642a8f32af
cdgraybill <cdgraybill@gmail.com>
2024-01-09 10:07:37 -0600

    day 1 part 2

--- TypeScript/src/2022/Solvers/Day01Solver.ts
+++ TypeScript/src/2022/Solvers/Day01Solver.ts
@@ -1,9 +1,17 @@
 import { parseToStrings } from "../Inputs/InputParser.js";
 
+var snacks = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
+
 function Day01Part01() {
-    var snacks = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
-    var temp = 0;
     var bags = [];
+
+    CalcualteBagWeights(bags);
+
+    return Math.max.apply(0, bags);
+} 
+
+function CalcualteBagWeights(bags: any[]) {
+    var temp = 0;
 
     for (const snack of snacks) {
         if (snack.length === 0) {
@@ -13,8 +21,34 @@ function Day01Part01() {
             temp += parseInt(snack);
         }
     }
+    return temp;
+}
 
-    return Math.max.apply(0, bags);
+function Day01Part02() {
+    var bags = [];
+
+    CalcualteBagWeights(bags);
+
+    var first = -1;
+    var second = -1;
+    var third = -1;
+
+    for (var bag of bags) {
+        if (bag > first) {
+            third = second;
+            second = first; 
+            first = bag; 
+        }
+        else if (bag > second) {
+            third = second;
+            second = bag; 
+        }
+        else if (bag > third){
+            third = bag;
+        }
+    }
+
+    return first + second + third;
 } 
 
-console.log(Day01Part01());
+console.log(Day01Part02());
