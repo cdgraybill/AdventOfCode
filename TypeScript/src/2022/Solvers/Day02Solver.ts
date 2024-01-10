7a6e913a158a7827ffad959198299c065b2edbad
cdgraybill <cdgraybill@gmail.com>
2024-01-10 10:58:44 -0600

    starting day 2

---
+++ TypeScript/src/2022/Solvers/Day02Solver.ts
@@ -0,0 +1,25 @@
+import { parseToStrings } from "../Inputs/InputParser.js";
+
+var rounds = parseToStrings("TypeScript/src/2022/Inputs/Day01Input.txt");
+
+function day02Part01() {
+    var score = 0;
+
+    var possibleOutcomes = {
+        "A X": 4,
+        "A Y": 8,
+        "A Z": 3,
+        "B X": 1,
+        "B Y": 5,
+        "B Z": 9,
+        "C X": 7,
+        "C Y": 2,
+        "C Z": 6,
+    }
+
+    for (const round of rounds) {
+        
+    }
+
+    return score;
+}
