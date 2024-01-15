883606bcdeb59d1f495ea94de530558eba9e5539
cdgraybill <cdgraybill@gmail.com>
2024-01-15 10:35:36 -0600

    working on day 5

--- TypeScript/src/2022/Solvers/Day05Solver.ts
+++ TypeScript/src/2022/Solvers/Day05Solver.ts
@@ -5,6 +5,7 @@ var input = parseToStrings("TypeScript/src/2022/Inputs/Day05Input.txt");
 function getBoxGrid(input: string[]) {
     var boxGrid = [];
 
+    // Let's get the box grid
     for (const line of input) {
         if (line.length !== 0) {
             boxGrid.push(line);
@@ -12,8 +13,25 @@ function getBoxGrid(input: string[]) {
             break;
         }
     }
+    
+    var columnsRow = boxGrid[boxGrid.length - 1];
+    var numberOfColumns = parseInt(columnsRow[columnsRow.length - 2]);
+    var parsedList = new Array<string>(numberOfColumns);
 
-    return boxGrid;
+    // And then parse it into an array of easily malleable strings
+    for (const line of boxGrid) {
+        for (let i = 1; i < line.length - 1; i += 4) {
+            var box = line[i];
+            var column = boxGrid[boxGrid.length - 1][i];
+            var parsedListIndex = parsedList[column - 1];
+
+            if (box != " ") {
+                parsedListIndex += box;
+            }
+        }
+    }
+
+    return parsedList;
 }
 
 console.log(getBoxGrid(input));
