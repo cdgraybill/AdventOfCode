f2c4d43538672cfc0c70b7d2a0c0243082e80264
cdgraybill <cdgraybill@gmail.com>
2024-01-21 20:55:37 -0600

    starting day 8 part 2

--- TypeScript/src/2022/Solvers/Day08Solver.ts
+++ TypeScript/src/2022/Solvers/Day08Solver.ts
@@ -35,4 +35,57 @@ function day08part01() {
     return answer;
 }
 
-console.log(day08part01());
+function day08part02() { 
+    var answer = 0;
+    var verticalLines = [];
+    var treeLine = [];
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input.length; j++) {
+            const tree = input[j][i];
+
+            treeLine.push(tree);
+        }
+        verticalLines.push(treeLine);
+        treeLine = [];
+    }
+
+    for (let k = 1; k < input.length - 1; k++) {
+        for (let l = 1; l < input[k].length - 1; l++) {
+            const tree = input[k][l];
+
+            var leftOfTree = getScenicScore(input[k].slice(0, l).reverse(), tree);
+            var aboveTree = getScenicScore(verticalLines[l].slice(0, k).reverse(), tree);
+            var rightOfTree = getScenicScore(input[k].slice(l + 1), tree);
+            var belowTree = getScenicScore(verticalLines[l].slice(k + 1), tree);
+
+            var totalScenicScore = leftOfTree * rightOfTree * aboveTree * belowTree;
+
+            if (totalScenicScore > answer) {
+                answer = totalScenicScore;
+            }
+        }
+    }
+
+    return answer;
+}
+
+// TODO need to get the scoring algorithim down
+function getScenicScore(treeLine: number[], currentTree: number) {
+    var score = 0;
+
+    for (const tree of treeLine) {
+        if (treeLine.length === 1) {
+            score++;
+            break;
+        } else if (currentTree > tree) {
+            score++;
+        } else {
+            break;
+        }
+    }
+
+    return score;
+}
+
+console.log(day08part02());
