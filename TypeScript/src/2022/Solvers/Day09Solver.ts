98bc3892e586d029aefc16d2b1103ec57f248136
cdgraybill <cdgraybill@gmail.com>
2024-01-24 09:16:37 -0600

    blah

--- TypeScript/src/2022/Solvers/Day09Solver.ts
+++ TypeScript/src/2022/Solvers/Day09Solver.ts
@@ -1,6 +1,6 @@
 import { parseToStrings } from "../Helpers/InputParser.js";
 
-var input = parseToStrings("ProblemInputs/2022/Day08Input.txt");
+var input = parseToStrings("ProblemInputs/2022/Day09Input.txt");
 
 function day09Part01() {
     var answer = 1; // starts at one to account for starting position
@@ -14,12 +14,16 @@ function day09Part01() {
             case "L":
                 break;
             case "R":
-                
+                grid[0][numberOfSteps - 1] = "H"
                 break;
             case "U":
                 break;
             case "D":
                 break;
         }
+
+
     }
 }
+
+console.log(day09Part01());
