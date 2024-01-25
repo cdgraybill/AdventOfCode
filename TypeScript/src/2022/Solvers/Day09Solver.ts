baa198405fbc2feb1f82dbcd2d44f1652c7b4e75
cdgraybill <cdgraybill@gmail.com>
2024-01-25 10:31:59 -0600

    i think we're getting somewhere

--- TypeScript/src/2022/Solvers/Day09Solver.ts
+++ TypeScript/src/2022/Solvers/Day09Solver.ts
@@ -4,7 +4,11 @@ var input = parseToStrings("ProblemInputs/2022/Day09Input.txt");
 
 function day09Part01() {
     var answer = 1; // starts at one to account for starting position
-    var grid = [];
+
+    //current plan: log a series of coordinates in an array, then count all unique coordinates
+    var allPositions = [];
+    var currentPosition = [0, 0];
+    var tempPosition = [];
 
     for (let i = 0; i < input.length; i++) {
         var direction = input[i][0];
@@ -13,17 +17,25 @@ function day09Part01() {
         switch (direction) {
             case "L":
                 break;
+
             case "R":
-                grid[0][numberOfSteps - 1] = "H"
+                for (let j = 0; j < numberOfSteps - 1; j++) {
+                    currentPosition[1]++;
+                    tempPosition = currentPosition;
+                    allPositions.push(tempPosition);
+                    tempPosition.length = 0;
+                }
                 break;
+
             case "U":
                 break;
+
             case "D":
                 break;
         }
-
-
     }
+
+    return allPositions;
 }
 
 console.log(day09Part01());
