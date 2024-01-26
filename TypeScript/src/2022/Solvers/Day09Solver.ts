d45cfe4627df7f053edf9ab41795d869bf3ca951
cdgraybill <cdgraybill@gmail.com>
2024-01-26 10:44:35 -0600

    more day 9

--- TypeScript/src/2022/Solvers/Day09Solver.ts
+++ TypeScript/src/2022/Solvers/Day09Solver.ts
@@ -5,10 +5,16 @@ var input = parseToStrings("ProblemInputs/2022/Day09Input.txt");
 function day09Part01() {
     var answer = 1; // starts at one to account for starting position
 
-    //current plan: log a series of coordinates in an array, then count all unique coordinates
+    //plan: log a series of coordinates in an array, then count all unique coordinates
+    //current task: figuring out the correct algorithim for each case
+    
     var allPositions = [];
-    var currentPosition = [0, 0];
-    var tempPosition = [];
+
+    var xPositionTail = 0;
+    var yPositionTail = 0;
+
+    var xPositionHead = 0;
+    var yPositionHead = 0;
 
     for (let i = 0; i < input.length; i++) {
         var direction = input[i][0];
@@ -16,21 +22,45 @@ function day09Part01() {
 
         switch (direction) {
             case "L":
-                break;
+                xPositionHead -= numberOfSteps;
 
-            case "R":
                 for (let j = 0; j < numberOfSteps - 1; j++) {
-                    currentPosition[1]++;
-                    tempPosition = currentPosition;
-                    allPositions.push(tempPosition);
-                    tempPosition.length = 0;
+                    xPositionTail--;
+                    allPositions.push([xPositionTail, yPositionTail]);
                 }
                 break;
 
+            case "R":
+                xPositionHead += numberOfSteps;
+
+                if ((xPositionHead - yPositionTail) > 1) {
+                    for (let j = 0; j < numberOfSteps - 1; j++) {
+                        xPositionTail++;
+                        allPositions.push([xPositionTail, yPositionTail]);
+                    }
+                }
+                
+                break;
+
             case "U":
+                yPositionHead += numberOfSteps;
+
+                if ((yPositionHead - xPositionTail) > 1) {
+                    for (let j = 0; j < numberOfSteps - 1; j++) {
+                        yPositionTail++;
+                        allPositions.push([xPositionTail, yPositionTail]);
+                    }
+                }
+
                 break;
 
             case "D":
+                yPositionHead -= numberOfSteps;
+                
+                for (let j = 0; j < numberOfSteps - 1; j++) {
+                    yPositionTail--;
+                    allPositions.push([xPositionTail, yPositionTail]);
+                }
                 break;
         }
     }
